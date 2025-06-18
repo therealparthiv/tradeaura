4a5de749b33cb2045fff2cfebaff815e86961d20
therealparthiv <writetoparthiv@gmail.com>
2025-06-18 20:30:28 +0530

    fix: re-added frontend folder as regular directory, not submodule

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1> 
+      <p> Heyyyyy </p>
+    </h1>
+  </React.StrictMode>
+);
