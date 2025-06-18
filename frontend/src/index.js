f6eee4f15c38d4627301a71c84b7064eeeb5481b
therealparthiv <writetoparthiv@gmail.com>
2025-06-18 20:59:07 +0530

    feat: added homepage component layout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import HomePage from "./landing_page/home/HomePage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1> 
-      <p> Heyyyyy </p>
-    </h1>
+    <HomePage />
   </React.StrictMode>
 );
