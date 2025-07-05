e1f67340db679f5042a176322368bda5062ace0e
therealparthiv <writetoparthiv@gmail.com>
2025-07-06 00:53:18 +0530

    Backend Initial Setup

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -1,11 +1,32 @@
-import React from "react";
-
-import { positions } from "../data/data";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { VerticalGraph } from "./VerticalGraph";
 
 const Positions = () => {
+  const [allPositions, setAllPositions] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/allPositions").then((res) => {
+      setAllPositions(res.data);
+    });
+  }, []);
+
+  const labels = allPositions.map((stock) => stock.name);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Stock Price",
+        data: allPositions.map((stock) => stock.price),
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
   return (
     <>
-      <h3 className="title">Positions ({positions.length})</h3>
+      <h3 className="title">Positions ({allPositions.length})</h3>
 
       <div className="order-table">
         <table>
@@ -13,13 +34,15 @@ const Positions = () => {
             <th>Product</th>
             <th>Instrument</th>
             <th>Qty.</th>
-            <th>Avg.</th>
+            <th>Avg. cost</th>
             <th>LTP</th>
+            <th>Cur. val</th>
             <th>P&L</th>
-            <th>Chg.</th>
+            <th>Net chg.</th>
+            <th>Day chg.</th>
           </tr>
 
-          {positions.map((stock, index) => {
+          {allPositions.map((stock, index) => {
             const curValue = stock.price * stock.qty;
             const isProfit = curValue - stock.avg * stock.qty >= 0.0;
             const profClass = isProfit ? "profit" : "loss";
@@ -32,15 +55,19 @@ const Positions = () => {
                 <td>{stock.qty}</td>
                 <td>{stock.avg.toFixed(2)}</td>
                 <td>{stock.price.toFixed(2)}</td>
+                <td>{curValue.toFixed(2)}</td>
                 <td className={profClass}>
                   {(curValue - stock.avg * stock.qty).toFixed(2)}
                 </td>
+                <td className={profClass}>{stock.net}</td>
                 <td className={dayClass}>{stock.day}</td>
               </tr>
             );
           })}
         </table>
       </div>
+
+      <VerticalGraph data={data} />
     </>
   );
 };
