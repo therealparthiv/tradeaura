090a86bfabc72b7fd47ecb26eb1748d56e148545
therealparthiv <writetoparthiv@gmail.com>
2025-08-09 23:59:52 +0530

    Working

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./index.css";
-import Home from "./components/Home";
 import { GeneralContextProvider } from "./components/GeneralContext";
+import Home from "./components/Home";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <GeneralContextProvider>
+        {/* Per your request, this file remains unchanged and correct. */}
         <Routes>
           <Route path="/*" element={<Home />} />
         </Routes>
