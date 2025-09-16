f82174fd1dce689e7dcd986801da53ac40db305f
therealparthiv <writetoparthiv@gmail.com>
2025-09-16 19:26:43 +0530

    deploying

--- dashboard/src/utils/axiosInstance.js
+++ dashboard/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3002",
+  baseURL: "https://tradeaura-backend.onrender.com",
   withCredentials: true, // ✅ always send cookies
 });
 
